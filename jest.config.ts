2361c8014dd743c2595230bc8c8825ec42abcc93
Julia Thea <julia.thea@protonmail.com>
2026-01-08 19:39:09 -0600

    Restructured to monorepo with nextjs apis

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './'
+});
+
+const config: Config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8'
+};
+
+export default createJestConfig(config);
