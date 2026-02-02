bd5c67cfba42d6390934b43422de3b4e0fd9af71
Julia Thea <julia.thea@protonmail.com>
2026-02-01 18:51:44 -0600

    fix

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-"use client"
+// import React from 'react';
+// "use client"
 
-export default function SignIn() {
-  return (
-    <form
-      action={async (formData) => {
-        "use server"
-        await signUp("credentials", formData)
-      }}
-    >
-      <label>
-        Email
-        <input name="email" type="email" />
-      </label>
-      <label>
-        Password
-        <input name="password" type="password" />
-      </label>
-      <button>Sign Up</button>
-    </form>
-  )
-}
+// export default function SignIn() {
+//   return (
+//     <form
+//       action={async (formData) => {
+//         "use server"
+//         await signUp("credentials", formData)
+//       }}
+//     >
+//       <label>
+//         Email
+//         <input name="email" type="email" />
+//       </label>
+//       <label>
+//         Password
+//         <input name="password" type="password" />
+//       </label>
+//       <button>Sign Up</button>
+//     </form>
+//   )
+// }
